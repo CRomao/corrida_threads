2936eb61127602e88794bf0114717196a603612d
CRomao <ciceroromao1923@gmail.com>
2020-03-09 08:26:30 -0300

    Adicionado resultado da corrida

--- Corrida.java
+++ Corrida.java
@@ -40,8 +40,11 @@ public class Corrida extends Thread {
     }
 
     public void rankingCompetidor(ArrayList<Competidor> lista) {
+        System.out.println("\nA corrida terminou!\n");
+        System.out.println("-=-=--=-=-Resultados da corrida-=-=--=-=-");
+        System.out.println("LUGAR\t|\tCOMPETIDOR\t|\tTEMPO DECORRIDO");
         for (int i = 0; i < competidores.size(); i++) {
-            System.out.println(competidores.get(i).getNome() + " | " + competidores.get(i).getHoraFinalizouCorrida() + " segundos");
+            System.out.printf("%dº\t|\t%s    \t|\t%d segundos\n", i+1, competidores.get(i).getNome(), competidores.get(i).getHoraFinalizouCorrida());
         }
     }
 
